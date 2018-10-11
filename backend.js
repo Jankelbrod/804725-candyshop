c87f38bf54396ca94a262a0cf952e7ba5359dd11
Jankelbrod <38419678+Jankelbrod@users.noreply.github.com>
2018-10-11 14:01:50 +0500

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,50 @@
+'use strict';
+
+(function () {
+
+  var URL_GET = 'https://js.dump.academy/candyshop/data';
+  var URL_POST = 'https://js.dump.academy/candyshop';
+  var SUCCESS_CODE = 200;
+  var TIMEOUT = 10000;
+
+  var createRequest = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_CODE) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT;
+    return xhr;
+  };
+
+  var load = function (onLoad, onError) {
+    var xhr = createRequest(onLoad, onError);
+    xhr.open('GET', URL_GET);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr = createRequest(onLoad, onError);
+    xhr.open('POST', URL_POST);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
